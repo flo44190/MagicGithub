73b571131a120347cd0aa7c6d596db705b8d6b0d
Florent Barbaud <barbaudflorent@gmail.com>
2020-04-29 17:02:15 +0200

    modif User Repositorie

--- app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
+++ app/src/main/java/com/openclassrooms/magicgithub/repository/UserRepository.java
@@ -1,7 +1,6 @@
 package com.openclassrooms.magicgithub.repository;
 import com.openclassrooms.magicgithub.api.ApiService;
 import com.openclassrooms.magicgithub.model.User;
-import static com.openclassrooms.magicgithub.api.FakeApiServiceGenerator.generateUsers;
 
 import java.util.List;
 
